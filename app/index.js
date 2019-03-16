492013a4c5259e68b4b1015171b330d7e437a045
LucasJotaR <luqas_erre@outlook.com>
2019-03-16 13:05:36 -0300

    Finalizando Node.js

--- app/index.js
+++ app/index.js
@@ -1,2 +1,11 @@
-var saludo = "Hola Mundo desde js!"
-console.log(saludo)
+//ES8: import http from "./http"
+let http = require("http")
+
+http.createServer(function(request, response){
+
+    var saludo = "Hola Mundo desde <strong>Node.js</strong>!"
+    response.setHeader("Content-Type","text/html")
+    response.write(saludo)
+    response.end(" Usted quiere este recurso: " + request.url)
+
+}).listen(2000)
